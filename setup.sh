9b6dce7385335ac32efd2d5993dbc65ee551cbfa
John <john@errant.tech>
2019-06-21 22:31:53 -0700

    Possibly working v0.0.1

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-yum install nginx
+yum install nginx -y
+sed -i /etc/nginx/nginx.conf -re '40,86d'
 cp example.conf /etc/nginx/conf.d
 
-sed -f sedswaps index.html
 mkdir /var/www/example
-mv index.html /var/www/example
+sed -e "s@BGCOLOR@$(tr -dc 'A-F0-9' < /dev/urandom | head -c6)@g; s@INSTANCE@$(curl http://169.254.169.254/latest/meta-data/instance-id)@g; s@IPPUB@$(curl http://169.254.169.254/latest/meta-data/public-ipv4)@g; s@IPPRIV@$(curl http://169.254.169.254/latest/meta-data/local-ipv4)@g; s@AZ@$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone)@g" index.template > /var/www/example/index.html
 
 service nginx restart
