ff4712ab4e03edc641f094c33fb4a9fceab28b40
John <john@errant.tech>
2019-06-21 22:11:03 -0700

    fixing sed stuff

--- sedswaps.sh
+++ sedswaps.sh
@@ -1,5 +1,5 @@
 sed "s@BGCOLOR@$(tr -dc 'A-F0-9' < /dev/urandom | head -c6)@g" index.html > undex.html
-sed "s@INSTANCE@$(curl http://169.254.169.254/latest/meta-data/instance-id)@g" index.html > undex.html
-sed "s@IPPUB@$(curl http://169.254.169.254/latest/meta-data/public-ipv4)@g" index.html > undex.html
-sed "s@IPPRIV@$(curl http://169.254.169.254/latest/meta-data/local-ipv4)@g" index.html > undex.html
-sed "s@AZ@$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone)@g" index.html > undex.html
+sed "s@INSTANCE@$(curl http://169.254.169.254/latest/meta-data/instance-id)@g" undex.html > undex.html
+sed "s@IPPUB@$(curl http://169.254.169.254/latest/meta-data/public-ipv4)@g" undex.html > undex.html
+sed "s@IPPRIV@$(curl http://169.254.169.254/latest/meta-data/local-ipv4)@g" undex.html > undex.html
+sed "s@AZ@$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone)@g" undex.html > undex.html
